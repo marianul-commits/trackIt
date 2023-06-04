547bccd9afe0214f09d3f7a4b660209a4a2aff19
marianul-commits <80788036+marianul-commits@users.noreply.github.com>
2023-06-04 03:19:35 +0300

    Almost v 1.0

--- kitchicUIKit/Model/Constants.swift
+++ kitchicUIKit/Model/Constants.swift
@@ -15,4 +15,8 @@ struct K {
 
 struct Values {
     static var searchItem:String = ""
+    static let healthNews = URL(string: "https://newsapi.org/v2/top-headlines/sources?category=healthapiKey=ec980cbd0b2a47faabe429dff372c708")
+    static let techNews = URL(string: "https://newsapi.org/v2/top-headlines/sources?category=technologyapiKey=ec980cbd0b2a47faabe429dff372c708")
+    static let sciNews = URL(string: "https://newsapi.org/v2/top-headlines/sources?category=scienceapiKey=ec980cbd0b2a47faabe429dff372c708")
+    static let sportNews = URL(string: "https://newsapi.org/v2/top-headlines/sources?category=sportsapiKey=ec980cbd0b2a47faabe429dff372c708")
 }
